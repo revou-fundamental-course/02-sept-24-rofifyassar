fc4e9270bcd06419c49bcd53c331477c073afd40
rofifyassar <rofifyassar2@gmail.com>
2024-09-11 10:43:13 +0700

    update design

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 //Ini Javascript
-function hitungLuas(){
-    
+function cekluas() {
+    const alas = parseFloat(document.getElementById('alas').value);
+    const tinggi = parseFloat(document.getElementById('tinggi').value);
+    const luas = (alas * tinggi) / 2;
+    tampilkanHasil(`Luas: ${luas.toFixed(2)}`);
 }
+
+function tampilkanHasil(pesan) {
+    document.getElementById('hasilluas').innerHTML = pesan;
+}
+
+document.querySelector('.resetluas-btn').addEventListener('click', function() {
+    document.getElementById('alas').value = '';
+    document.getElementById('tinggi').value = '';
+    document.getElementById('hasilluas').innerHTML = '';
+});
+
